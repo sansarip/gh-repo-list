7c9d434e3f07b51f25b4f4e2eb813b9fdd4ba468
sepehr ansaripour <pehrans@gmail.com>
2019-11-27 00:06:35 -0500

    Remove unnecessary templates

--- src/cljs/my_website/views/about/panel.cljs
+++
@@ -1,8 +0,0 @@
-(ns my-website.views.about.panel)
-
-(defn about-panel []
-      [:div
-       [:h1 "This is the About Page."]
-       [:div
-        [:a {:href "#/"}
-         "go to Home Page"]]])

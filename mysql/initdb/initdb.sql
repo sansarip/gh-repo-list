b8f8aa6e61e16e619942b9acf00ae367ecfbf525
sepehr ansaripour <pehrans@gmail.com>
2019-11-28 14:28:20 -0500

    Update Repository schema

--- mysql/initdb/initdb.sql
+++ mysql/initdb/initdb.sql
@@ -1,16 +1,16 @@
 CREATE DATABASE TestApp;
 
-CREATE TABLE TestApp.Repositories (id int(10) not null, name varchar(256), stars int(10) not null);
+CREATE TABLE TestApp.Repositories (id int NOT NULL AUTO_INCREMENT, name varchar(256), stars int(10) NOT NULL, PRIMARY KEY (id));
 
-CREATE TABLE TestApp.Template (title varchar(256) not null, subtitle varchar(256) not null, isActive bit(1) not null);
+CREATE TABLE TestApp.Template (title varchar(256) NOT NULL, subtitle varchar(256) NOT NULL, isActive bit(1) NOT NULL);
 
-INSERT INTO TestApp.Repositories
-VALUES (0, 'Front End SPA', 3),
-       (1, 'Web API', 5),
-       (2, 'Orm', 1),
-       (3, 'Business Tier', 2),
-       (4, 'Native Android App', 3),
-       (5, 'Native IOS App', 1);
+INSERT INTO TestApp.Repositories (name, stars)
+VALUES ('Front End SPA', 3),
+       ('Web API', 5),
+       ('Orm', 1),
+       ('Business Tier', 2),
+       ('Native Android App', 3),
+       ('Native IOS App', 1);
 
 INSERT INTO TestApp.Template
 VALUES ('Test Repository Viewer', 'This is where the magic happens', b'1'),
